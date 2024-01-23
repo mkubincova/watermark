70ed2d84f81a24c144f3c118a1848996fcac584f
mkubincova <mkubincova@proton.me>
2024-01-23 15:39:21 +0100

    feat: watermark customization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import colorchooser
 import os
 
 ALLOWED_FILETYPES = [("PNG files", "*.png"), ("JPEG files", "*.jpg *.jpeg")]
@@ -17,22 +18,73 @@ class WatermarkApp:
 
         self.canvas = tk.Canvas(self.root, width=700, height=700, bg="black", highlightthickness=0, borderwidth=0)
         self.watermark = self.canvas.create_text(350, 350, anchor=tk.CENTER, text="YOUR WATERMARK", font=("Courier", 35, "bold"), fill="gray")
-        self.canvas.grid(row=1, column=0, pady=10)
+        self.canvas.grid(row=1, column=0, rowspan=20, pady=10, padx=10)
 
         self.watermark_label = tk.Label(text="Your watermark:")
-        self.watermark_label.grid(row=0, column=1, pady=10, sticky="W")
-
+        self.watermark_label.grid(row=1, column=1, sticky="W")
         self.watermark_input = tk.Entry()
-        self.watermark_input.grid(row=1, column=1, pady=10, sticky="N")
+        self.watermark_input.grid(row=2, column=1, sticky="NW")
         self.watermark_input.focus()
-        self.watermark_input.bind('<KeyRelease>', self.on_change)
+        self.watermark_input.bind('<KeyRelease>', self.on_input_change)
+
+        self.font_size_label = tk.Label(text="Font size:")
+        self.font_size_label.grid(row=3, column=1, sticky="W")
+        self.font_size_input = tk.Entry()
+        self.font_size_input.grid(row=4, column=1, sticky="NW")
+        self.font_size_input.bind('<KeyRelease>', self.on_size_change)
+
+        self.select_color_label = tk.Label(text="Watermark color:")
+        self.select_color_label.grid(row=5, column=1, sticky="W")
+        self.select_color_button = tk.Button(self.root, text='Select a Color', command=self.on_color_change)
+        self.select_color_button.grid(row=6, column=1, sticky="NW")
+
+        self.watermark_label = tk.Label(text="Alignment:")
+        self.watermark_label.grid(row=7, column=1, sticky="W")
+        self.options = {
+            "center": {"x": 350, "y": 350, "anchor": "center"},
+            "top left": {"x": 30, "y": 30, "anchor": "nw"},
+            "top right": {"x": 670, "y": 30, "anchor": "ne"},
+            "bottom left": {"x": 30, "y": 670, "anchor": "sw"},
+            "bottom right": {"x": 670, "y": 670, "anchor": "se"},
+        }
+        self.position = tk.StringVar(self.root)
+        self.position.set("center")  # default value
+        self.position.trace("w", self.on_position_change)
+        self.select_position = tk.OptionMenu(self.root, self.position, *self.options.keys())
+        self.select_position.grid(row=8, column=1, sticky="NW")
+
+        self.angle_label = tk.Label(text="Rotation angle (deg):")
+        self.angle_label.grid(row=9, column=1, sticky="W")
+        self.angle_input = tk.Entry()
+        self.angle_input.grid(row=10, column=1, sticky="NW")
+        self.angle_input.bind('<KeyRelease>', self.on_angle_change)
 
         self.save_button = tk.Button(self.root, text="Save Image", command=self.save_canvas)
-        self.save_button.grid(row=2, column=1, pady=10, sticky="S")
+        self.save_button.grid(row=11, column=1,  sticky="NW")
 
-    def on_change(self, event):
+    def on_input_change(self, event):
         watermark_text = self.watermark_input.get()
         self.canvas.itemconfig(self.watermark, text=watermark_text)
+
+    def on_color_change(self):
+        color_code = tk.colorchooser.askcolor(title=r"Choose color")
+        self.canvas.itemconfig(self.watermark, fill=color_code[1])
+
+    def on_angle_change(self, event):
+        angle = self.angle_input.get()
+        if angle.isdigit():
+            self.canvas.itemconfig(self.watermark, angle=float(angle))
+
+    def on_size_change(self, event):
+        size = self.font_size_input.get()
+        if size.isdigit():
+            self.canvas.itemconfig(self.watermark, font=("Courier", int(size), "bold"))
+
+    def on_position_change(self, var, *args):
+        position_name = self.root.getvar(var)
+        position_vals = self.options[position_name]
+        self.canvas.itemconfig(self.watermark, anchor=position_vals["anchor"])
+        self.canvas.coords(self.watermark, position_vals["x"], position_vals["y"])
 
     def center_window(self):
         window_height = 1000
@@ -44,7 +96,7 @@ class WatermarkApp:
         self.root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cor, y_cor))
 
     def load_image(self):
-        file_path = filedialog.askopenfilename(initialdir=os.path.join(os.path.expanduser("~"), "Desktop", "projekty"), filetypes=ALLOWED_FILETYPES)
+        file_path = filedialog.askopenfilename(initialdir=os.path.join(os.path.expanduser("~"), "Desktop"), filetypes=ALLOWED_FILETYPES)
 
         if file_path:
             # Load the original image
@@ -75,7 +127,6 @@ class WatermarkApp:
     def display_image(self, image):
         # Convert the Pillow image to a PhotoImage object for display in Tkinter canvas
         tk_image = ImageTk.PhotoImage(image)
-        print(image.width)
         # Update the canvas with the new image and place watermark on top
         self.canvas.config(width=image.width, height=image.height)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=tk_image)
